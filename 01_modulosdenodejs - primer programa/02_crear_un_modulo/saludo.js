3e609dcd334e7568416771081d36b549651e7ddf
Joao Garduño <joao.garduno@gmail.com>
2024-01-02 16:06:02 -0600

    Estructura 2: Modulos de Node.js y primer programa - Crear módulo

---
+++ 01_modulosdenodejs - primer programa/02_crear_un_modulo/saludo.js
@@ -0,0 +1,6 @@
+// Tenemos 2 modulos uno con su definición de la función y en el otro modulo vamos a llamar a esa función
+function saludar(nombre){
+  return `Hola, ${nombre}`;
+}
+
+// Entonces estamos ocupando un modulo en el cual tenga las definiciones y podemos reeusar esa definiciones donde queramos en los otros modulos de la aplicación
